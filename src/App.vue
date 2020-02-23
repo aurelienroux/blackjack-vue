b19a19715e7cb0eb4e9238d73e86d01a552524fa
Aurélien Roux <aureroux13@gmail.com>
2020-02-23 09:43:19 -0500

    player stay method

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <p>score {{ dealerScore }}</p>
     <div class="dealer-section">
       <div v-for="(card, index) in dealerCards" :key="index">
-        <img :src="card.image" alt="" />
+        <img v-if="index === 0 && !dealerPlay" src="@/assets/card.png" alt="" />
+        <img v-else-if="index === 0 && dealerPlay" :src="card.image" alt="" />
+        <img v-else :src="card.image" alt="dealer card" />
       </div>
     </div>
 
     <button @click="newHand">New hand</button>
     <button @click="hitMe" :disabled="!gamePlaying">Hit me</button>
+    <button @click="stay" :disabled="!gamePlaying">I stay</button>
     <p>game playing {{ gamePlaying }}</p>
 
     <p>score {{ playerScore }}</p>
     <div class="player-section">
       <div v-for="(card, index) in playerCards" :key="index">
-        <img :src="card.image" alt="" />
+        <img :src="card.image" alt="player card" />
       </div>
     </div>
   </div>
@@ -32,46 +35,44 @@ export default {
       playerCards: [],
       playerScore: 0,
       dealerCards: [],
-      dealerScore: 0
+      dealerScore: 0,
+      dealerPlay: false
     }
   },
   methods: {
-    dealCard(card, person) {
-      if (person === 'dealer') {
-        this.dealerCards.push(card)
+    computeScore(cards, score) {
+      let hasAce = false
+
+      score = cards.reduce((acc, card) => {
         if (card.value === 'ACE') {
-          this.dealerScore += 11
-        } else if (card.value === 'JACK' || card.value === 'QUEEN' || card.value === 'KING') {
-          this.dealerScore += 10
-        } else {
-          this.dealerScore += Number(card.value)
+          hasAce = true
+          return acc + 1
         }
-      } else {
-        this.playerCards.push(card)
-        if (card.value === 'ACE') {
-          this.playerScore += 11
-        } else if (card.value === 'JACK' || card.value === 'QUEEN' || card.value === 'KING') {
-          this.playerScore += 10
-        } else {
-          this.playerScore += Number(card.value)
+        if (isNaN(card.value)) {
+          return acc + 10
         }
+        return acc + Number(card.value)
+      }, 0)
+
+      if (hasAce) {
+        score = score + 10 > 21 ? score : score + 10
       }
+
+      return score
     },
     newHand() {
-      axios.get(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=4`).then(response => {
+      axios.get(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=4`).then(res => {
         this.gamePlaying = true
         this.playerCards = []
         this.dealerCards = []
         this.playerScore = 0
         this.dealerScore = 0
 
-        response.data.cards.forEach((card, index) => {
-          if (index % 2 === 0) {
-            this.dealCard(card, 'dealer')
-          } else {
-            this.dealCard(card, 'player')
-          }
-        })
+        this.playerCards.push(res.data.cards[0], res.data.cards[2])
+        this.dealerCards.push(res.data.cards[1], res.data.cards[3])
+
+        this.playerScore = this.computeScore(this.playerCards, this.playerScore)
+        this.dealerScore = this.computeScore(this.dealerCards, this.dealerScore)
       })
     },
     hitMe() {
@@ -79,13 +80,18 @@ export default {
         .get(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`)
         .then(response => {
           const [card] = response.data.cards
-          this.dealCard(card, 'player')
+
+          this.playerCards.push(card)
+          this.playerScore = this.computeScore(this.playerCards, this.playerScore)
 
           if (this.playerScore > 21) {
             this.gamePlaying = false
           }
         })
         .catch(error => console.log(error))
+    },
+    stay() {
+      this.dealerPlay = true
     }
   },
   created() {
