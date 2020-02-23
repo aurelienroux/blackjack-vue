c11a20122038d5e2848cb9b55f76b280f65f41c4
Aurélien Roux <aureroux13@gmail.com>
2020-02-22 20:34:40 -0500

    create newHand and hitMe method

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    {{ deckId }}
+    <p>score {{ dealerScore }}</p>
+    <div class="dealer-section">
+      <div v-for="(card, index) in dealerCards" :key="index">
+        <img :src="card.image" alt="" />
+        <p>{{ card.value }}</p>
+      </div>
+    </div>
+
+    <button @click="newHand">New hand</button>
+    <button @click="hitMe">Hit me</button>
+
+    <p>score {{ playerScore }}</p>
+    <div class="player-section">
+      <div v-for="(card, index) in playerCards" :key="index">
+        <img :src="card.image" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,14 +27,67 @@ export default {
   name: 'App',
   data() {
     return {
-      deckId: ''
+      deckId: '',
+      playerCards: [],
+      playerScore: 0,
+      dealerCards: [],
+      dealerScore: 0
+    }
+  },
+  methods: {
+    newHand() {
+      axios.get(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=4`).then(response => {
+        this.playerCards = []
+        this.playerScore = 0
+        this.dealerCards = []
+        this.dealerScore = 0
+
+        const cards = response.data.cards
+
+        cards.forEach((card, index) => {
+          if (index % 2 === 0) {
+            this.dealerCards.push(card)
+
+            if (card.value === 'ACE') {
+              this.dealerScore += 11
+            } else if (card.value === 'JACK' || card.value === 'QUEEN' || card.value === 'KING') {
+              this.dealerScore += 10
+            } else {
+              this.dealerScore += Number(card.value)
+            }
+          } else {
+            this.playerCards.push(card)
+
+            if (card.value === 'ACE') {
+              this.playerScore += 11
+            } else if (card.value === 'JACK' || card.value === 'QUEEN' || card.value === 'KING') {
+              this.playerScore += 10
+            } else {
+              this.playerScore += Number(card.value)
+            }
+          }
+        })
+      })
+    },
+    hitMe() {
+      axios.get(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`).then(response => {
+        const [card] = response.data.cards
+        this.playerCards.push(card)
+
+        if (card.value === 'ACE') {
+          this.playerScore += 11
+        } else if (card.value === 'JACK' || card.value === 'QUEEN' || card.value === 'KING') {
+          this.playerScore += 10
+        } else {
+          this.playerScore += Number(card.value)
+        }
+      })
     }
   },
   created() {
     axios
       .get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2')
       .then(response => {
-        console.log(response)
         this.deckId = response.data.deck_id
       })
       .catch(error => console.log(error))
@@ -27,12 +96,16 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.dealer-section,
+.player-section {
+  border: 1px solid;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1rem;
+}
+
+img {
+  margin: 0.25rem;
+  max-height: 120px;
 }
 </style>
