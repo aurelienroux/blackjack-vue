bdb047248c380647d633c710a4da29f2d85cb6a8
Aurélien Roux <aurelien.roux@aerial.ai>
2020-12-11 19:16:34 -0500

    dealer & feedback tests

---
+++ tests/unit/components/dealer.spec.js
@@ -0,0 +1,88 @@
+import { shallowMount } from '@vue/test-utils'
+import Dealer from '@/components/Dealer.vue'
+
+function createLocalWrapper(gamePlaying = false, dealerScore = 0, dealerCards = []) {
+  return shallowMount(Dealer, {
+    propsData: {
+      gamePlaying: gamePlaying,
+      dealerScore: dealerScore,
+      dealerCards: dealerCards
+    }
+  })
+}
+
+const dealerCards = [
+  {
+    code: '5C',
+    image: 'https://deckofcardsapi.com/static/img/5C.png',
+    images: {
+      svg: 'https://deckofcardsapi.com/static/img/5C.svg',
+      png: 'https://deckofcardsapi.com/static/img/5C.png'
+    },
+    value: '5',
+    suit: 'CLUBS'
+  },
+  {
+    code: '3S',
+    image: 'https://deckofcardsapi.com/static/img/3S.png',
+    images: {
+      svg: 'https://deckofcardsapi.com/static/img/3S.svg',
+      png: 'https://deckofcardsapi.com/static/img/3S.png'
+    },
+    value: '3',
+    suit: 'SPADES'
+  }
+]
+
+describe('Dealer component', () => {
+  describe('default state', () => {
+    it('renders', () => {
+      const wrapper = createLocalWrapper()
+      expect(wrapper).toMatchSnapshot()
+      expect(wrapper.find('.dealer').exists()).toBe(true)
+      expect(wrapper.find('p').text()).toMatch('Dealer: 0')
+    })
+  })
+
+  describe('playing game', () => {
+    it('renders', () => {
+      const wrapper = createLocalWrapper(true, 8, dealerCards)
+      expect(wrapper).toMatchSnapshot()
+      expect(wrapper.find('p').text()).toMatch('Dealer: ?')
+      expect(wrapper.findAll('img').length).toBe(2)
+      expect(
+        wrapper
+          .findAll('img')
+          .at(0)
+          .attributes('src')
+      ).toBe('@/assets/card.png')
+      expect(
+        wrapper
+          .findAll('img')
+          .at(1)
+          .attributes('src')
+      ).toBe('https://deckofcardsapi.com/static/img/3S.png')
+    })
+  })
+
+  describe('game over', () => {
+    it('renders', () => {
+      const wrapper = createLocalWrapper(false, 20, dealerCards)
+      expect(wrapper).toMatchSnapshot()
+      expect(wrapper.find('p').text()).toMatch('Dealer: 20')
+      expect(wrapper.findAll('img').length).toBe(2)
+      expect(
+        wrapper
+          .findAll('img')
+          .at(0)
+          .attributes('src')
+      ).toBe('https://deckofcardsapi.com/static/img/5C.png')
+      expect(
+        wrapper
+          .findAll('img')
+          .at(1)
+          .attributes('src')
+      ).toBe('https://deckofcardsapi.com/static/img/3S.png')
+    })
+  })
+})
---
+++ tests/unit/components/feedback.spec.js
@@ -0,0 +1,43 @@
+import { shallowMount } from '@vue/test-utils'
+import Feedback from '@/components/Feedback.vue'
+
+function createLocalWrapper(loading = false, gamePlaying = false, feedback = '') {
+  return shallowMount(Feedback, {
+    propsData: {
+      loading: loading,
+      gamePlaying: gamePlaying,
+      feedback: feedback
+    }
+  })
+}
+
+describe('Feedback component', () => {
+  describe('default state', () => {
+    it('renders', () => {
+      const wrapper = createLocalWrapper()
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+
+  describe('loading', () => {
+    it('renders', () => {
+      const wrapper = createLocalWrapper(true)
+      expect(wrapper).toMatchSnapshot()
+      expect(
+        wrapper
+          .findAll('p')
+          .at(0)
+          .text()
+      ).toMatch('Loading...')
+    })
+  })
+
+  describe('game over', () => {
+    it('renders', () => {
+      const feedbackText = 'feedback text'
+      const wrapper = createLocalWrapper(false, false, feedbackText)
+      expect(wrapper).toMatchSnapshot()
+      expect(wrapper.find('p').text()).toMatch(feedbackText)
+    })
+  })
+})
